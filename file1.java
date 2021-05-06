367bddd27465a067a2c42a5703d3ab1c2132ec62
Yogapriya <ypriya.in@gmail.com>
2021-05-06 14:50:04 +0000

    Reverted Java Code
    This reverts commit a16e86631d6ed1d36df88658971972a53c59676a.

---
+++ file1.java
@@ -0,0 +1,19 @@
+class simple
+public void feature1()
+{
+	system.out.println("Code for feature 1");
+}
+public void feature2()
+{
+	system.out.println("Code for feature 2");
+}
+public void feature3()
+{
+	system.out.println("Code for feature 3");
+}
+public void feature4()
+{
+	system.out.println("Added more Code");
+}
+
+
