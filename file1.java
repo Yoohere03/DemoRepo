7a11bda48e9b4bbc522c05cec2281f8b8ff8a9be
Yogapriya <ypriya.in@gmail.com>
2021-05-06 13:38:20 +0000

    New files added

---
+++ file1.java
@@ -0,0 +1,14 @@
+class simple
+public void feature1()
+{
+	system.out.println("Code for feature 1");
+}
+public void feature2()
+{
+	system.out.println("Code for feature 2");
+}
+public void feature3()
+{
+	system.out.println("Code for feature 3");
+}
+
